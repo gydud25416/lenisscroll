7f0cbd58dea43072ad85bce851bb7e73814c0155
gydud25416 <gydud25416@gmail.com>
2024-07-30 22:58:23 +0900

    feat: GSAP 가로 세로 스크롤 섞기
    feat: GSAP 가로 세로 스크롤 섞기

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -15,6 +15,8 @@ export default function Home() {
   const sectionRef = useRef(null); // useRef로 참조할 요소
   const triggerRef = useRef(null);
   const welcomeRef = useRef(null);
+  const workRef = useRef(null);
+  const contackRef = useRef(null);
 
   // horizontal 스크롤 애니메이션
   useEffect(() => {
@@ -22,12 +24,12 @@ export default function Home() {
       sectionRef.current, // Gsap 애니메이션이 시작되는 요소 위치
       { translateX: 0 }, // from 부분(초기 시작)
       {
-        translateX: -6000, // to 부분
+        translateX: -4500, // to 부분, 최종 상태
         ease: "none", // 쓸데없는 애니메이션 없애는 부분
         scrollTrigger: { // 스크롤 애니메이션 발생하는 부분
           trigger: triggerRef.current, // 스크롤이 발생되는 요소 위치
           start: "top top", // "요소위치 시작위치"
-          end: "+=8000", // "요소위치 끝위치"
+          end: "+=4500", // "요소위치 끝위치"
           scrub: 0.7, // 되감기 기능, 또한 스크롤을 부드러운 애니메이션 추가.
           pin: "#port", // 가로스크롤시 페이지를 고정할 수 있는 기능
         },
@@ -51,37 +53,63 @@ export default function Home() {
           duration: 1,
           scrollTrigger: {
             trigger: welcomeRef.current,
-            start: '2000px', // 애니메이션 시작 위치
+            start: '1000px', // 애니메이션 시작 위치
             end: '+=500', // 애니메이션 종료 위치
             scrub: true // 스크롤과 함께 애니메이션 동기화
           }
         }
       );
     }, []);
- 
+   
+    useEffect(()=>{
+      const pin2 = gsap.fromTo(contackRef.current,
+        {position:"fixed", left:"100%", top:0 },
+        {
+          position:"fixed", left:0, // to 부분, 최종 상태
+        ease: "none", // 쓸데없는 애니메이션 없애는 부분
+        scrollTrigger: { // 스크롤 애니메이션 발생하는 부분
+          trigger: workRef.current, // 스크롤이 발생되는 요소 위치
+          start: "bottom bottom", // "요소위치 시작위치"
+          end: "bottom top", // "요소위치 끝위치"
+          scrub: 0.7, // 되감기 기능, 또한 스크롤을 부드러운 애니메이션 추가.
+          pin: workRef.current, // 가로스크롤시 페이지를 고정할 수 있는 기능
+        } 
+        }
+      )
+      return () => {
+        pin2.kill(); // 모든 애니메이션 중단
+      };
+    },[])
 
   return (
     <>
     <header id="header" role="banner"   ref={headerRef} >
-    <Header />
-  </header>
-      <div id="port" ref={triggerRef}>
-          {/* <Skip /> */}
-          <section ref={sectionRef}>
-            
-            <div className="scroll_wrap" > 
+      <Header />
+    </header>
+    <div id="port" ref={triggerRef}  >
+        <section ref={sectionRef} >
+          <div className="scroll_wrap" >
               <Intro /> 
-              <main id="main" role="main" > 
+              <main id="main" role="main"  > 
               <div className="wrap_welcome"   ref={welcomeRef}  > 
                     어서오세요
               </div> 
                   <Skill /> 
                   <Work />
-                  <Contact />
               </main> 
             </div>
           </section>
+          
       </div>
+          <section  className="second_scroll" >
+            <div ref={workRef}>
+              <Work   />
+            </div>
+            <div ref={contackRef}>
+              <Contact />
+            </div>
+          </section>
+          
       </>
   );
 
